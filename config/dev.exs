059ae832a0181a60d848f4dfeab4eac5de97fdb0
Dennis Peterson <denispeterson96@gmail.com>
2019-04-26 14:40:17 +0300

    Created and setup Postgresql database

--- config/dev.exs
+++ config/dev.exs
@@ -69,7 +69,7 @@ config :phoenix, :plug_init_mode, :runtime
 # Configure your database
 config :hello_pheonix, HelloPheonix.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "Dennis5050",
   database: "hello_pheonix_dev",
   hostname: "localhost",
   pool_size: 10
